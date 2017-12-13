b5602743a476d14818900a1b07578e86bd9ee6da
emmadae <emmadae@gmail.com>
2017-12-13 10:16:23 -0600

    safty updates

---
+++ orginizational-cal/client/src/components/ItemComponents/Email/Email.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Email.css";
+
+const Email = () =>
+
+<div>
+	<a href="#">
+		<p> You Have 3 Emails <i class="fa fa-envelope-o" aria-hidden="true"></i> </p>
+	</a>
+</div>
+
+export default Email;
